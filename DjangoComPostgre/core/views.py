691460ad132f03bc14486862f25dfac877ccd66b
Gabriel Pires <gabrielpirestj@hotmail.com>
2022-03-03 16:25:47 -0300

    Avanço aulas Django com Postgree
    Layout importado, models criados, views ja sendo exibidas.

--- DjangoComPostgre/core/views.py
+++ DjangoComPostgre/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Servico, Funcionario
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    """ 
+    Na função abaixo estamos recuperando o contexto ja existente get_context_data
+    e abaixo adicionamos os dados que queremos = context['servicos'] e context['funcionarios'] no caso estes
+    estao retornando do banco de dados Servico.objects.all() e por ultimo retornamos o contexto.
+    """
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('servico').all()
+        context['funcionarios'] = Funcionario.objects.all()
+        return context
+
+
